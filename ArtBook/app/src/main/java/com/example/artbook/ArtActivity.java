1135ba591febf177e0e883028367a1ee22cee364
Enes <98012557+enesaks@users.noreply.github.com>
2023-06-26 18:14:37 +0300

    ArtBook

---
+++ ArtBook/app/src/main/java/com/example/artbook/ArtActivity.java
@@ -0,0 +1,14 @@
+package com.example.artbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+}
