00585397537dcd4bb13cb74a848e8f2c591ba538
Enes <98012557+enesaks@users.noreply.github.com>
2023-06-26 22:03:43 +0300

    ArtBook

--- ArtBook/app/src/main/java/com/example/artbook/ArtActivity.java
+++ ArtBook/app/src/main/java/com/example/artbook/ArtActivity.java
@@ -1,14 +1,174 @@
 package com.example.artbook;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.accessibility.AccessibilityRequestPreparer;
+import android.widget.Toast;
+
+import com.example.artbook.databinding.ActivityArtBinding;
+import com.example.artbook.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.ByteArrayOutputStream;
 
 public class ArtActivity extends AppCompatActivity {
+
+    private ActivityArtBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Bitmap selectedImage;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art);
+        binding = ActivityArtBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        registerLauncher();
+    }
+    public void save (View view){
+        String artName = binding.nameText.getText().toString();
+        String artisName = binding.artisText.getText().toString();
+        String artYear = binding.yearText.getText().toString();
+
+        Bitmap smallIMage = makeSmallerImage(selectedImage,300);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        smallIMage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[] byteArray =  outputStream.toByteArray();
+
+        try {
+            database = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts(id INTEGER PRIMARY KEY, artname VARCHAR, paintername VARCHAR, year VARCHAR, image BLOB)");
+
+            String sqlString = "INSERT INTO arts(artname, paintername, year, image) VALUES(?, ?, ?, ?)";
+            SQLiteStatement  sqLiteStatement = database.compileStatement(sqlString);
+
+            sqLiteStatement.bindString(1,artName);
+            sqLiteStatement.bindString(2,artisName);
+            sqLiteStatement.bindString(3,artYear);
+            sqLiteStatement.bindBlob(4,byteArray);
+            sqLiteStatement.execute();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(ArtActivity.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+
+    public Bitmap makeSmallerImage(Bitmap image,int maxmumsize){
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / height;
+
+        if (bitmapRatio > 1 ){
+            //Landscape image
+            width = maxmumsize;
+            height = (int) (width/bitmapRatio);
+        }else{
+            //portrait image
+            height = maxmumsize;
+            width = (int) (height*bitmapRatio);
+        }
+        return image.createScaledBitmap(image,width,height,true);
+    }
+    public void selectImage (View view){
+
+        if (ContextCompat.checkSelfPermission(ArtActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Pirmission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Request permission
+                        permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }
+            else {
+                //Request permission
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+        else {
+            //gallary
+            Intent intentToGallert = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallert);
+        }
+
+
+    }
+
+    private void registerLauncher(){
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null){
+                        Uri imageData = intentFromResult.getData();
+                        //binding.imageView.setImageURI(imageData);
+
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28){
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(),imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }else{
+                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    //permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }else {
+                    //permission denied
+                    Toast.makeText(ArtActivity.this, "Permission needed!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
--- ArtBook/app/src/main/java/com/example/artbook/MainActivity.java
+++ ArtBook/app/src/main/java/com/example/artbook/MainActivity.java
@@ -1,19 +1,67 @@
 package com.example.artbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.artbook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        getData();
+
+
+    }
+
+    private void getData(){
+
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts",null);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_art ){
+            Intent intent = new Intent(this,ArtActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
