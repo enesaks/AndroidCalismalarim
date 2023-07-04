84cf4f7e36aad5f792adee1b2cae2af21156c4a8
Enes <98012557+enesaks@users.noreply.github.com>
2023-07-04 16:51:44 +0300

    SeyahatKitabım

---
+++ SeyahatKitapim/app/src/main/java/com/example/seyahatkitapim/MapsActivity.java
@@ -0,0 +1,60 @@
+package com.example.seyahatkitapim;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.seyahatkitapim.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                System.out.println("Location : "+ location.toString());
+
+            }
+
+        };
+
+        LatLng eiffel = new LatLng(48.85847219487556, 2.294425083899672);
+        mMap.addMarker(new MarkerOptions().position(eiffel).title("Eiffel Tower"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel,15));
+
+    }
+}
