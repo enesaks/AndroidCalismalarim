4e8a5a08cec164a41f27f478208b434c57e8c903
Enes <98012557+enesaks@users.noreply.github.com>
2023-07-13 20:12:30 +0300

    FragmentCalismasi

--- FragmentCalismasi/app/src/main/java/com/enesaksu/fragmentcalismasi/MainActivity.java
+++ FragmentCalismasi/app/src/main/java/com/enesaksu/fragmentcalismasi/MainActivity.java
@@ -1,14 +1,42 @@
 package com.enesaksu.fragmentcalismasi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void goToFirstFragment(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.fragment,firstFragment).commit();
+        fragmentTransaction.replace(R.id.fragment,firstFragment).commit();
+
+    }
+    public void goToSecondFragment(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        //fragmentTransaction.add(R.id.fragment,secondFragment).commit();
+        fragmentTransaction.replace(R.id.fragment,secondFragment).commit();
+
+    }
+
+
+
 }
